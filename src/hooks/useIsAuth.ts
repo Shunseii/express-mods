53cdd5eff8b19a654e597be4270a7f0faa986f12
Sufyan <khan.sufyan.cs@gmail.com>
2021-05-13 16:01:41 -0400

    Creates custom auth hook

---
+++ src/hooks/useIsAuth.ts
@@ -0,0 +1,17 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+import { useMeQuery } from "../generated/graphql";
+
+const useIsAuth = () => {
+  const [{ data, fetching }] = useMeQuery();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!fetching && !data?.me) {
+      router.replace("/login?next=" + router.asPath);
+    }
+  }, [fetching, data, router]);
+};
+
+export default useIsAuth;
