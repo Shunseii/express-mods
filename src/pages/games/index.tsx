65295eb06e2091658c08ea6b74706ce9c06f2cc9
Sufyan <khan.sufyan.cs@gmail.com>
2021-05-13 16:08:22 -0400

    Creates games index page

---
+++ src/pages/games/index.tsx
@@ -0,0 +1,41 @@
+import { NextPage } from "next";
+import { withUrqlClient } from "next-urql";
+import Head from "next/head";
+import React from "react";
+
+import Container from "../../components/Container";
+import Link from "../../components/Link";
+import Navbar from "../../components/Navbar";
+import { useGamesQuery } from "../../generated/graphql";
+import { createUrqlClient } from "../../utils/createUrqlClient";
+
+interface GamesPageProps {}
+
+const GamesPage: NextPage<GamesPageProps> = () => {
+  const [{ data, fetching }] = useGamesQuery();
+
+  if (fetching) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Games | Express Mods</title>
+        <link rel="icon" href="/files-circle.svg" />
+      </Head>
+
+      <Navbar />
+      <Container>
+        <h1 className="text-2xl font-medium">All games:</h1>
+        {data.games.map((game) => (
+          <Link key={game.id} href={`/games/${game.gameSlug}/mods`}>
+            {game.name}
+          </Link>
+        ))}
+      </Container>
+    </>
+  );
+};
+
+export default withUrqlClient(createUrqlClient, { ssr: true })(GamesPage);
