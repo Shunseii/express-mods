d9dbddcf5ae0a39a932061ea7f2be925e48f5549
Sufyan <khan.sufyan.cs@gmail.com>
2021-07-01 23:02:38 -0400

    Allows users to update mod desc

--- src/pages/games/[gameSlug]/mods/[modId].tsx
+++ src/pages/games/[gameSlug]/mods/[modId].tsx
@@ -25,8 +25,10 @@ interface ModPageProps {}
 
 const ModPage: NextPage<ModPageProps> = ({}) => {
   const [isEditingTitle, setIsEditingTitle] = useState(false);
+  const [isEditingContent, setIsEditingContent] = useState(false);
+
   const router = useRouter();
-  const modId = parseInt(router.query.gameId as string);
+  const modId = parseInt(router.query.modId as string);
   const [, updateMod] = useUpdateModMutation();
   const [{ data, fetching }] = useModQuery({ variables: { modId } });
 
@@ -48,10 +50,9 @@ const ModPage: NextPage<ModPageProps> = ({}) => {
       <Navbar />
       <Container>
         <div>
-          {data.mod.isOwner && isEditingTitle ? (
+          {data.mod.isOwner && isEditingTitle && (
             <Formik
               initialValues={{
-                content: data.mod.content,
                 title: data.mod.title,
               }}
               onSubmit={async (values, { setErrors }) => {
@@ -65,14 +66,8 @@ const ModPage: NextPage<ModPageProps> = ({}) => {
                   modVars.title = values.title;
                 }
 
-                if (values.content !== data.mod.content) {
-                  modVars.content = values.content;
-                }
-
                 const response = await updateMod(modVars);
                 const errors = mapAPIErrors(response.error?.graphQLErrors);
-
-                console.log(response);
 
                 if (errors) {
                   setErrors(errors.validation);
@@ -106,24 +101,100 @@ const ModPage: NextPage<ModPageProps> = ({}) => {
                 </Form>
               )}
             </Formik>
-          ) : (
+          )}
+
+          {!isEditingTitle && (
             <>
               <h1 className="mb-4 text-3xl">{data.mod.title}</h1>
-              {data.mod.isOwner && !isEditingTitle && (
+              {data.mod.isOwner && (
                 <PrimaryActionButton
-                  label="Edit"
+                  label="Edit title"
                   onClick={() => {
                     setIsEditingTitle(true);
                   }}
                 />
               )}
-              {!data.mod.isOwner && (
-                <p>
-                  by <span>{data.mod.author.username}</span>
-                </p>
-              )}
             </>
           )}
+
+          {!data.mod.isOwner && (
+            <p>
+              by <span>{data.mod.author.username}</span>
+            </p>
+          )}
+
+          <hr className="my-8" />
+
+          <div className="mt-12">
+            {!isEditingContent && (
+              <>
+                <p>{data.mod.content}</p>
+                {data.mod.isOwner && (
+                  <PrimaryActionButton
+                    className="mt-4"
+                    label="Edit description"
+                    onClick={() => {
+                      setIsEditingContent(true);
+                    }}
+                  />
+                )}
+              </>
+            )}
+
+            {data.mod.isOwner && isEditingContent && (
+              <Formik
+                initialValues={{
+                  content: data.mod.content,
+                }}
+                onSubmit={async (values, { setErrors }) => {
+                  await sleep(500);
+
+                  const modVars: UpdateModMutationVariables = {
+                    modId: data.mod.id,
+                  };
+
+                  if (values.content !== data.mod.content) {
+                    modVars.content = values.content;
+                  }
+
+                  const response = await updateMod(modVars);
+                  const errors = mapAPIErrors(response.error?.graphQLErrors);
+
+                  if (errors) {
+                    setErrors(errors.validation);
+                  } else {
+                    setIsEditingContent(false);
+                  }
+                }}
+              >
+                {({ isSubmitting }) => (
+                  <Form className="">
+                    <LabeledFormField
+                      className="mb-6"
+                      name="content"
+                      label="Content"
+                      type="textarea"
+                    />
+                    <div>
+                      <PrimaryActionButton
+                        isLoading={isSubmitting}
+                        className="mr-2 text-lg h-11"
+                        type="submit"
+                        label="Confirm"
+                      />
+                      <SecondaryActionButton
+                        className="text-lg h-11"
+                        label="Cancel"
+                        onClick={() => {
+                          setIsEditingContent(false);
+                        }}
+                      />
+                    </div>
+                  </Form>
+                )}
+              </Formik>
+            )}
+          </div>
         </div>
       </Container>
     </div>
