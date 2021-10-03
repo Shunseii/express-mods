4195808fa3e0b405b1d98e733ae7b9a4357c7fb3
Sufyan <khan.sufyan.cs@gmail.com>
2021-10-03 04:48:18 -0400

    chore: twin macro set up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
+import React from "react";
 import { AppProps } from "next/app";
 
+import GlobalStyles from "../../styles/GlobalStyles";
 import "../../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
+      <GlobalStyles />
       <Component {...pageProps} />
     </div>
   );
